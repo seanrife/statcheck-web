97ab47c2152227c544712eafdc8bbcd263fbed0f
Sean C. Rife <sean@rife.ws>
2016-06-27 15:44:33 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,41 @@
+library('shiny')
+library('statcheck')
+
+shinyUI(pageWithSidebar(
+  
+  # Header:
+  headerPanel("statcheck web interface"),
+  
+  # Input in sidepanel:
+  sidebarPanel(
+      
+    # Input:
+    fileInput("files", "Upload files (pdf or html):", multiple = TRUE, accept= c('pdf/html')),
+    
+    br(),
+    
+    radioButtons("outtype", "Show:",
+                 list("Plot" = "plot",
+                      "Summary table" = "tab",
+                      "Detailed" = "detail")
+    ),
+    
+    htmlOutput("selectpaper"),
+    
+    br(),
+    
+    downloadLink('downloadData', 'Download full report (csv)') ,
+    
+    br(),
+    
+    htmlOutput("legendText"),
+    
+    htmlOutput("legendField")
+    
+  ),
+  
+  # Plot in main:
+  mainPanel(
+    htmlOutput("window")
+  )
+))
